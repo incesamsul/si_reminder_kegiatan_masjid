f78e68b162f5721528851e0b53749e8da9af7682
incesamsul <ince.samsul89@gmail.com>
2023-07-22 21:10:16 +0800

    fix role and add something in home page

--- resources/views/layouts/v_nav.blade.php
+++ resources/views/layouts/v_nav.blade.php
@@ -6,8 +6,11 @@
                 <form class="form-inline mr-auto" method="POST" action="{{ URL::to('ubah_role') }}">
                     @csrf
                     <ul class="navbar-nav mr-3">
-                        <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg collapse-trigger-button"><i class="fas fa-bars"></i></a></li>
-                        <li><a href="#" data-toggle="search" class="nav-link nav-link-lg d-sm-none"><i class="fas fa-search"></i></a></li>
+                        <li><a href="#" data-toggle="sidebar"
+                                class="nav-link nav-link-lg collapse-trigger-button"><i class="fas fa-bars"></i></a>
+                        </li>
+                        <li><a href="#" data-toggle="search" class="nav-link nav-link-lg d-sm-none"><i
+                                    class="fas fa-search"></i></a></li>
                     </ul>
                     <div class="search-element">
 
@@ -15,7 +18,8 @@
                 </form>
                 <ul class="navbar-nav navbar-right">
 
-                    <li class="dropdown dropdown-list-toggle"><a href="#" data-toggle="dropdown" class="nav-link notification-toggle nav-link-lg beep"><i class="far fa-bell"></i></a>
+                    <li class="dropdown dropdown-list-toggle"><a href="#" data-toggle="dropdown"
+                            class="nav-link notification-toggle nav-link-lg beep"><i class="far fa-bell"></i></a>
                         <div class="dropdown-menu dropdown-list dropdown-menu-right">
                             <div class="dropdown-header">Berita terbaru
                                 <div class="float-right">
@@ -39,13 +43,15 @@
                             </div>
                         </div>
                     </li>
-                    <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
-                            <img  src="{{ auth()->user()->foto == "" ? asset('stisla/assets/img/avatar/avatar-1.png') : asset('data/foto_profile/'.auth()->user()->foto . '/'. auth()->user()->foto) }}" alt="" class="rounded-circle-profile mb-2" width="100">
+                    <li class="dropdown"><a href="#" data-toggle="dropdown"
+                            class="nav-link dropdown-toggle nav-link-lg nav-link-user">
+                            <img src="{{ auth()->user()->foto == '' ? asset('stisla/assets/img/avatar/avatar-1.png') : asset('data/foto_profile/' . auth()->user()->foto . '/' . auth()->user()->foto) }}"
+                                alt="" class="rounded-circle-profile mb-2" width="100">
                             <div class="d-sm-none d-lg-inline-block">{{ auth()->user()->name }}</div>
                         </a>
                         <div class="dropdown-menu dropdown-menu-right">
                             <div class="dropdown-title">{{ auth()->user()->role }}</div>
-                            <a href="features-profile.html" class="dropdown-item has-icon">
+                            {{-- <a href="features-profile.html" class="dropdown-item has-icon">
                                 <i class="far fa-user"></i> Profile
                             </a>
                             <a href="features-activities.html" class="dropdown-item has-icon">
@@ -54,7 +60,7 @@
                             <a href="features-settings.html" class="dropdown-item has-icon">
                                 <i class="fas fa-cog"></i> Settings
                             </a>
-                            <div class="dropdown-divider"></div>
+                            <div class="dropdown-divider"></div> --}}
                             <a href="{{ URL::to('/logout') }}" class="dropdown-item has-icon text-danger">
                                 <i class="fas fa-sign-out-alt"></i> Logout
                             </a>
