9a2b1a20e58076556a0af72952677e5b9425ed94
incesamsul <ince.samsul89@gmail.com>
2023-08-12 23:33:08 +0800

    add cdownload and filter

--- app/Http/Controllers/RekapKasController.php
+++ app/Http/Controllers/RekapKasController.php
@@ -10,8 +10,10 @@ class RekapKasController extends Controller
 {
     public function index()
     {
-        $data['kas_keluar'] = KasKeluar::all();
-        $data['kas_masuk'] = KasMasuk::all();
+        $tgl_mulai = isset($_GET['tgl_mulai']) ? $_GET['tgl_mulai'] : '';
+        $tgl_akhir = isset($_GET['tgl_akhir']) ? $_GET['tgl_akhir'] : '';
+        $data['kas_keluar'] = KasKeluar::whereBetween('tanggal', [$tgl_mulai, $tgl_akhir])->get();
+        $data['kas_masuk'] = KasMasuk::whereBetween('tanggal', [$tgl_mulai, $tgl_akhir])->get();
         return view('pages.rekap_kas.index', $data);
     }
 }
--- resources/views/pages/rekap_kas/index.blade.php
+++ resources/views/pages/rekap_kas/index.blade.php
@@ -3,6 +3,31 @@
 @section('content')
     <section class="section">
         <div class="row">
+            <div class="col-sm-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h5>Filter</h5>
+                    </div>
+                    <div class="card-body ">
+                        <form action="" class="row">
+                            <div class="form-group col-sm-6">
+                                <label for="tgl_mulai">tgl mulai</label>
+                                <input type="date" class="form-control " name="tgl_mulai">
+                            </div>
+                            <div class="form-group col-sm-6">
+                                <label for="tgl_akhir">tgl akhir</label>
+                                <input type="date" class="form-control " name="tgl_akhir">
+                            </div>
+                            <div class="form-group col-sm-12">
+                                <button type="submit" class="btn bg-main text-white"><i class="fas fa-sync"></i>
+                                    filter </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
             <div class="col-lg-12">
                 <div class="card">
                     <div class="card-header d-flex  justify-content-between">
@@ -50,7 +75,7 @@
                                     <div class="card-body">
 
                                         <table class="table table-striped table-hover table-user table-action-hover"
-                                            id="table-data">
+                                            id="table-data-blank">
                                             <thead>
                                                 <tr>
                                                     <th>#</th>
